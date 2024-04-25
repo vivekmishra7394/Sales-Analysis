dd7a238312b1d0f51b709a74704411a21ea9e59d
vivekmishra7394 <109514162+vivekmishra7394@users.noreply.github.com>
2024-04-25 21:22:04 +0530

    dim_product.sql

---
+++ dim_product.sql
@@ -0,0 +1,26 @@
+
+SELECT 
+  p.[ProductKey], 
+  p.[ProductAlternateKey] AS ProductItemCode, 
+
+  p.[EnglishProductName] AS [Product Name], 
+  ps.EnglishProductSubcategoryName AS [Sub Category], 
+  pc.EnglishProductCategoryName AS [Product Category], 
+
+  p.[Color] AS [Product Color], 
+
+  p.[Size] AS [Product Size], 
+
+  p.[ProductLine] AS [Product Line], 
+
+  p.[ModelName] AS [Product Model Name], 
+
+  p.[EnglishDescription] AS [Product Description], 
+
+  ISNULL (p.Status, 'Outdated') AS [Product Status] 
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimProduct] as p
+  LEFT JOIN dbo.DimProductSubcategory AS ps ON ps.ProductSubcategoryKey = p.ProductSubcategoryKey 
+  LEFT JOIN dbo.DimProductCategory AS pc ON ps.ProductCategoryKey = pc.ProductCategoryKey 
+order by 
+  p.ProductKey asc
