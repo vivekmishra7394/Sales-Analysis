4162a3d85edae83fb2fbb388d227b192ae98d231
vivekmishra7394 <109514162+vivekmishra7394@users.noreply.github.com>
2024-04-25 21:20:22 +0530

    dim_customer.sql

---
+++ dim_customer.sql
@@ -0,0 +1,15 @@
+--Required Columns
+
+SELECT 
+  c.customerkey AS CustomerKey, 
+  c.firstname AS [First Name], 
+  c.lastname AS [Last Name], 
+  c.firstname + ' ' + c.lastname AS [Full Name], 
+  CASE c.gender WHEN 'M' THEN 'Male' WHEN 'F' THEN 'Female' END AS Gender,
+  c.datefirstpurchase AS DateFirstPurchase, 
+  g.city AS [Customer City] 
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimCustomer] as c
+  LEFT JOIN dbo.dimgeography AS g ON g.geographykey = c.geographykey 
+ORDER BY 
+  CustomerKey ASC 
